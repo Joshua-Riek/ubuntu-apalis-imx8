f69329c5cc4c5e418507b86a54a24dca3cf989f7
Joshua-Riek <jjriek@verizon.net>
2022-05-05 18:01:32 -0400

    add patch for kernel header install

--- build-kernel.sh
+++ build-kernel.sh
@@ -15,6 +15,12 @@ if [ ! -d linux-toradex ]; then
     git clone --progress -b toradex_5.4-2.1.x-imx git://git.toradex.com/linux-toradex.git
 fi
 cd linux-toradex
+
+# Apply patch for leak during kernel headers install
+if git apply --check ../../patch/701-net-0408-sdk_fman-fix-CONFIG_COMPAT-leak-during-headers-insta.patch > /dev/null 2>&1; then
+    git apply ../../patch/701-net-0408-sdk_fman-fix-CONFIG_COMPAT-leak-during-headers-insta.patch
+fi
+
 make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- distclean
 make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- defconfig
 ./scripts/config --set-val CONFIG_DEBUG_INFO n
@@ -23,4 +29,4 @@ echo "-toradex" > .scmversion
 
 make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- -j $(nproc)
 make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- -j $(nproc) DTC_FLAGS="-@" dtbs
-make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- -j $(nproc) bindeb-pkg -i
+make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- -j $(nproc) bindeb-pkg
