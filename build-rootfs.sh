7308b99cc73dee5036a038f2ad4e0294bf4ba347
Joshua Riek <jjriek@verizon.net>
2022-05-19 14:53:54 -0400

    code refactor and dircolors revert

--- build-rootfs.sh
+++ build-rootfs.sh
@@ -52,9 +52,8 @@ mount -o bind /dev/pts $chroot_dir/dev/pts
 # Copy kernel to the rootfs
 cp ./*.deb $chroot_dir/tmp
 
-# Run the inital chroot script to setup os env
+# Download and update packages
 cat << EOF | chroot $chroot_dir /bin/bash
-#!/bin/bash
 set -eE 
 trap 'echo Error: in $0 on line $LINENO' ERR
 
@@ -86,15 +85,12 @@ net-tools wireless-tools openssh-client openssh-server wpasupplicant ifupdown
 
 # Clean package cache
 apt-get autoremove -y && apt-get clean -y && apt-get autoclean -y
+EOF
 
-# Download and extract hdmi firmware
-mkdir -p /tmp/imx-seco && cd /tmp/imx-seco
-wget -nc https://www.nxp.com/lgfiles/NMG/MAD/YOCTO/firmware-imx-8.15.bin
-chmod u+x firmware-imx-8.15.bin
-./firmware-imx-8.15.bin --auto-accept --force
-mkdir -p /lib/firmware/imx/hdmi
-cp /tmp/imx-seco/firmware-imx-8.15/firmware/hdmi/cadence/* /lib/firmware/imx/hdmi
-cd / && rm -rf /tmp/imx-seco
+# Create user accounts
+cat << EOF | chroot $chroot_dir /bin/bash
+set -eE 
+trap 'echo Error: in $0 on line $LINENO' ERR
 
 # Setup user account
 adduser --shell /bin/bash --gecos ubuntu --disabled-password ubuntu
@@ -105,22 +101,35 @@ echo -e "root\nroot" | passwd ubuntu
 
 # Root pass
 echo -e "root\nroot" | passwd
+EOF
+
+# Grab the kernel version
+kernel_version="$(cat linux-toradex/include/generated/utsrelease.h | sed -e 's/.*"\(.*\)".*/\1/')"
+
+# Install kernel and create initramfs
+cat << EOF | chroot $chroot_dir /bin/bash
+set -eE 
+trap 'echo Error: in $0 on line $LINENO' ERR
 
 # Install kernel
 dpkg -i /tmp/*.deb
 rm -rf /tmp/*
 
+# Generate kernel module dependencies
+depmod -a ${kernel_version}
+
 # Update initramfs
 update-initramfs -u
+EOF
 
 # DNS
-echo "nameserver 8.8.8.8" > /etc/resolv.conf
+echo "nameserver 8.8.8.8" > $chroot_dir/etc/resolv.conf
 
 # Hostname
-echo "apalis-imx8qm" > /etc/hostname
+echo "apalis-imx8qm" > $chroot_dir/etc/hostname
 
 # Networking interfaces
-cat << END > /etc/network/interfaces
+cat > $chroot_dir/etc/network/interfaces << END
 auto lo
 iface lo inet loopback
 
@@ -133,7 +142,7 @@ iface wlan0 inet dhcp
 END
 
 # Hosts file
-cat << END > /etc/hosts
+cat > $chroot_dir/etc/hosts << END
 127.0.0.1       localhost
 127.0.1.1       apalis-imx8qm
 
@@ -145,7 +154,7 @@ ff02::3         ip6-allhosts
 END
 
 # WIFI
-cat << END > /etc/wpa_supplicant/wpa_supplicant.conf
+cat > $chroot_dir/etc/wpa_supplicant/wpa_supplicant.conf << END
 ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
 update_config=1
 country=US
@@ -157,37 +166,177 @@ network={
 }
 END
 
-# Terminal colors for ls
-LS_COLORS='rs=0:di=01;34:ln=01;36:mh=00:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;
-01:or=40;31;01:mi=00:su=37;41:sg=30;43:ca=30;41:tw=30;42:ow=34;42:st=37;44:ex=01;32:*.tar=01;
-31:*.tgz=01;31:*.arc=01;31:*.arj=01;31:*.taz=01;31:*.lha=01;31:*.lz4=01;31:*.lzh=01;
-31:*.lzma=01;31:*.tlz=01;31:*.txz=01;31:*.tzo=01;31:*.t7z=01;31:*.zip=01;31:*.z=01;
-31:*.dz=01;31:*.gz=01;31:*.lrz=01;31:*.lz=01;31:*.lzo=01;31:*.xz=01;31:*.zst=01;31:*.tzst=01;
-31:*.bz2=01;31:*.bz=01;31:*.tbz=01;31:*.tbz2=01;31:*.tz=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;
-31:*.war=01;31:*.ear=01;31:*.sar=01;31:*.rar=01;31:*.alz=01;31:*.ace=01;31:*.zoo=01;31:*.cpio=01;
-31:*.7z=01;31:*.rz=01;31:*.cab=01;31:*.wim=01;31:*.swm=01;31:*.dwm=01;31:*.esd=01;31:*.jpg=01;
-35:*.jpeg=01;35:*.mjpg=01;35:*.mjpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;
-35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.svg=01;
-35:*.svgz=01;35:*.mng=01;35:*.pcx=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.m2v=01;35:*.mkv=01;
-35:*.webm=01;35:*.ogm=01;35:*.mp4=01;35:*.m4v=01;35:*.mp4v=01;35:*.vob=01;35:*.qt=01;35:*.nuv=01;
-35:*.wmv=01;35:*.asf=01;35:*.rm=01;35:*.rmvb=01;35:*.flc=01;35:*.avi=01;35:*.fli=01;35:*.flv=01;
-35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.yuv=01;35:*.cgm=01;35:*.emf=01;35:*.ogv=01;
-35:*.ogx=01;35:*.aac=00;36:*.au=00;36:*.flac=00;36:*.m4a=00;36:*.mid=00;36:*.midi=00;36:*.mka=00;
-36:*.mp3=00;36:*.mpc=00;36:*.ogg=00;36:*.ra=00;36:*.wav=00;36:*.oga=00;36:*.opus=00;36:*.spx=00;
-36:*.xspf=00;36:'
+# Terminal dircolors
+tee $chroot_dir/home/ubuntu/.dircolors $chroot_dir/root/.dircolors &>/dev/null << END
+# below are the color init strings for the basic file types
+RESET 0
+DIR 01;34
+LINK 01;36
+# numerical value, the color is as for the file pointed to.)
+MULTIHARDLINK 00
+FIFO 40;33
+SOCK 01;35
+DOOR 01;35
+BLK 40;33;01
+CHR 40;33;01
+ORPHAN 40;31;01
+MISSING 00
+SETUID 37;41
+SETGID 30;43
+CAPABILITY 30;41
+STICKY_OTHER_WRITABLE 30;42
+OTHER_WRITABLE 34;42
+STICKY 37;44
+# this is for files with execute permission
+EXEC 01;32
+# archives or compressed (bright red)
+.tar 01;31
+.tgz 01;31
+.arc 01;31
+.arj 01;31
+.taz 01;31
+.lha 01;31
+.lz4 01;31
+.lzh 01;31
+.lzma 01;31
+.tlz 01;31
+.txz 01;31
+.tzo 01;31
+.t7z 01;31
+.zip 01;31
+.z 01;31
+.dz 01;31
+.gz 01;31
+.lrz 01;31
+.lz 01;31
+.lzo 01;31
+.xz 01;31
+.zst 01;31
+.tzst 01;31
+.bz2 01;31
+.bz 01;31
+.tbz 01;31
+.tbz2 01;31
+.tz 01;31
+.deb 01;31
+.rpm 01;31
+.jar 01;31
+.war 01;31
+.ear 01;31
+.sar 01;31
+.rar 01;31
+.alz 01;31
+.ace 01;31
+.zoo 01;31
+.cpio 01;31
+.7z 01;31
+.rz 01;31
+.cab 01;31
+.wim 01;31
+.swm 01;31
+.dwm 01;31
+.esd 01;31
+# image formats
+.jpg 01;35
+.jpeg 01;35
+.mjpg 01;35
+.mjpeg 01;35
+.gif 01;35
+.bmp 01;35
+.pbm 01;35
+.pgm 01;35
+.ppm 01;35
+.tga 01;35
+.xbm 01;35
+.xpm 01;35
+.tif 01;35
+.tiff 01;35
+.png 01;35
+.svg 01;35
+.svgz 01;35
+.mng 01;35
+.pcx 01;35
+.mov 01;35
+.mpg 01;35
+.mpeg 01;35
+.m2v 01;35
+.mkv 01;35
+.webm 01;35
+.ogm 01;35
+.mp4 01;35
+.m4v 01;35
+.mp4v 01;35
+.vob 01;35
+.qt 01;35
+.nuv 01;35
+.wmv 01;35
+.asf 01;35
+.rm 01;35
+.rmvb 01;35
+.flc 01;35
+.avi 01;35
+.fli 01;35
+.flv 01;35
+.gl 01;35
+.dl 01;35
+.xcf 01;35
+.xwd 01;35
+.yuv 01;35
+.cgm 01;35
+.emf 01;35
+# https://wiki.xiph.org/MIME_Types_and_File_Extensions
+.ogv 01;35
+.ogx 01;35
+# audio formats
+.aac 00;36
+.au 00;36
+.flac 00;36
+.m4a 00;36
+.mid 00;36
+.midi 00;36
+.mka 00;36
+.mp3 00;36
+.mpc 00;36
+.ogg 00;36
+.ra 00;36
+.wav 00;36
+# https://wiki.xiph.org/MIME_Types_and_File_Extensions
+.oga 00;36
+.opus 00;36
+.spx 00;36
+.xspf 00;36
+# termcap
+TERM Eterm
+TERM ansi
+TERM *color*
+TERM con[0-9]*x[0-9]*
+TERM cons25
+TERM console
+TERM cygwin
+TERM dtterm
+TERM gnome
+TERM hurd
+TERM jfbterm
+TERM konsole
+TERM kterm
+TERM linux
+TERM linux-c
+TERM mlterm
+TERM putty
+TERM rxvt*
+TERM screen*
+TERM st
+TERM terminator
+TERM tmux*
+TERM vt100
+TERM vt220
+TERM xterm*
+END
+sed -i 's/#force_color_prompt=yes/color_prompt=yes/g' $chroot_dir/home/ubuntu/.bashrc
 
-# Set ls colors
-echo "export LS_COLORS='$LS_COLORS'" > /root/.lscolors
-echo "export LS_COLORS='$LS_COLORS'" > /home/ubuntu/.lscolors
-echo "if [ -f /root/.lscolors ]; then . /root/.lscolors; fi" >> /root/.bashrc
-echo "if [ -f /home/ubuntu/.lscolors ]; then . /home/ubuntu/.lscolors; fi" >> /home/ubuntu/.bashrc
-sed -i 's/#force_color_prompt=yes/force_color_prompt=yes/g' /home/ubuntu/.bashrc
-
-# Enable serial console on ttyLP1
-sed -i '/Service/aEnvironment=TERM=linux' /lib/systemd/system/serial-getty@.service
-systemctl enable serial-getty@ttyLP1
-
-EOF
+# Copy the hdmi firmware
+mkdir -p $chroot_dir/lib/firmware/imx/hdmi
+cp imx-seco/firmware-imx-8.15/firmware/hdmi/cadence/* $chroot_dir/lib/firmware/imx/hdmi
 
 # Umount the temporary API filesystems
 umount -lf $chroot_dir/dev/pts 2> /dev/null || true
