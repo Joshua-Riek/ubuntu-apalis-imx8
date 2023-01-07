2fb3d80b5b52cb4c189e6eaa1e32ce7338c739f9
Joshua Riek <jjriek@verizon.net>
2023-01-06 22:31:50 -0500

    remove custom ubuntu images

--- scripts/build-rootfs.sh
+++ scripts/build-rootfs.sh
@@ -129,6 +129,11 @@ p7zip-full htop iotop pciutils lshw lsof cryptsetup exfat-fuse hwinfo dmidecode 
 net-tools wireless-tools openssh-client openssh-server wpasupplicant ifupdown \
 pigz wget curl grub-common grub2-common grub-efi-arm64 grub-efi-arm64-bin
 
+# Download and install developer packages
+DEBIAN_FRONTEND=noninteractive apt-get -y --no-install-recommends install \
+git binutils build-essential bc bison cmake flex libssl-dev device-tree-compiler \
+i2c-tools u-boot-tools binfmt-support
+
 # Clean package cache
 apt-get -y autoremove && apt-get -y clean && apt-get -y autoclean
 EOF
@@ -328,6 +333,7 @@ umount -lf ${chroot_dir}/* 2> /dev/null || true
 cd ${chroot_dir} && XZ_OPT="-0 -T0" tar -cpJf ../ubuntu-20.04-preinstalled-server-arm64-apalis.rootfs.tar.xz . && cd ..
 
 # Mount the temporary API filesystems
+mkdir -p ${chroot_dir}/{proc,sys,run,dev,dev/pts}
 mount -t proc /proc ${chroot_dir}/proc
 mount -t sysfs /sys ${chroot_dir}/sys
 mount -o bind /dev ${chroot_dir}/dev
@@ -335,252 +341,6 @@ mount -o bind /dev/pts ${chroot_dir}/dev/pts
 
 # Download and update packages
 cat << EOF | chroot ${chroot_dir} /bin/bash
-set -eE 
-trap 'echo Error: in $0 on line $LINENO' ERR
-
-# Developer packages
-DEBIAN_FRONTEND=noninteractive apt-get -y --no-install-recommends install \
-git binutils build-essential bc bison cmake flex libssl-dev device-tree-compiler \
-i2c-tools u-boot-tools binfmt-support
-
-# Clean package cache
-apt-get -y autoremove && apt-get -y clean && apt-get -y autoclean
-EOF
-
-# Terminal dircolors
-tee ${chroot_dir}/home/ubuntu/.dircolors ${chroot_dir}/root/.dircolors &>/dev/null << END
-# Core formats
-RESET 0
-DIR 01;34
-LINK 01;36
-MULTIHARDLINK 00
-FIFO 40;33
-SOCK 01;35
-DOOR 01;35
-BLK 40;33;01
-CHR 40;33;01
-ORPHAN 40;31;01
-MISSING 00
-SETUID 37;41
-SETGID 30;43
-CAPABILITY 30;41
-STICKY_OTHER_WRITABLE 30;42
-OTHER_WRITABLE 34;42
-STICKY 37;44
-EXEC 01;32
-# Archive formats
-*.7z 01;31
-*.arj 01;31
-*.bz2 01;31
-*.cpio 01;31
-*.gz 01;31
-*.lrz 01;31
-*.lz 01;31
-*.lzma 01;31
-*.lzo 01;31
-*.rar 01;31
-*.s7z 01;31
-*.sz 01;31
-*.tar 01;31
-*.tbz 01;31
-*.tgz 01;31
-*.warc 01;31
-*.WARC 01;31
-*.xz 01;31
-*.z 01;31
-*.zip 01;31
-*.zipx 01;31
-*.zoo 01;31
-*.zpaq 01;31
-*.zst 01;31
-*.zstd 01;31
-*.zz 01;31
-# Packaged app formats
-.apk 01;31
-.ipa 01;31
-.deb 01;31
-.rpm 01;31
-.jad 01;31
-.jar 01;31
-.ear 01;31
-.war 01;31
-.cab 01;31
-.pak 01;31
-.pk3 01;31
-.vdf 01;31
-.vpk 01;31
-.bsp 01;31
-.dmg 01;31
-.crx 01;31
-.xpi 01;31
-# Image formats
-.bmp 01;35
-.dicom 01;35
-.tiff 01;35
-.tif 01;35
-.TIFF 01;35
-.cdr 01;35
-.flif 01;35
-.gif 01;35
-.icns 01;35
-.ico 01;35
-.jpeg 01;35
-.JPG 01;35
-.jpg 01;35
-.nth 01;35
-.png 01;35
-.psd 01;35
-.pxd 01;35
-.pxm 01;35
-.xpm 01;35
-.webp 01;35
-.ai 01;35
-.eps 01;35
-.epsf 01;35
-.drw 01;35
-.ps 01;35
-.svg 01;35
-# Audio formats
-.3ga 01;35
-.S3M 01;35
-.aac 01;35
-.amr 01;35
-.au 01;35
-.caf 01;35
-.dat 01;35
-.dts 01;35
-.fcm 01;35
-.m4a 01;35
-.mod 01;35
-.mp3 01;35
-.mp4a 01;35
-.oga 01;35
-.ogg 01;35
-.opus 01;35
-.s3m 01;35
-.sid 01;35
-.wma 01;35
-.ape 01;35
-.aiff 01;35
-.cda 01;35
-.flac 01;35
-.alac 01;35
-.mid 01;35
-.midi 01;35
-.pcm 01;35
-.wav 01;35
-.wv 01;35
-.wvc 01;35
-.ogv 01;35
-.ogx 01;35
-# Video formats
-.avi 01;35
-.divx 01;35
-.IFO 01;35
-.m2v 01;35
-.m4v 01;35
-.mkv 01;35
-.MOV 01;35
-.mov 01;35
-.mp4 01;35
-.mpeg 01;35
-.mpg 01;35
-.ogm 01;35
-.rmvb 01;35
-.sample 01;35
-.wmv 01;35
-.3g2 01;35
-.3gp 01;35
-.gp3 01;35
-.webm 01;35
-.gp4 01;35
-.asf 01;35
-.flv 01;35
-.ts 01;35
-.ogv 01;35
-.f4v 01;35
-.VOB 01;35
-.vob 01;35
-# Termcap
-TERM ansi
-TERM color-xterm
-TERM con132x25
-TERM con132x30
-TERM con132x43
-TERM con132x60
-TERM con80x25
-TERM con80x28
-TERM con80x30
-TERM con80x43
-TERM con80x50
-TERM con80x60
-TERM cons25
-TERM console
-TERM cygwin
-TERM dtterm
-TERM Eterm
-TERM eterm-color
-TERM gnome
-TERM gnome-256color
-TERM jfbterm
-TERM konsole
-TERM kterm
-TERM linux
-TERM linux-c
-TERM mach-color
-TERM mlterm
-TERM putty
-TERM rxvt
-TERM rxvt-256color
-TERM rxvt-cygwin
-TERM rxvt-cygwin-native
-TERM rxvt-unicode
-TERM rxvt-unicode-256color
-TERM rxvt-unicode256
-TERM screen
-TERM screen-256color
-TERM screen-256color-bce
-TERM screen-bce
-TERM screen-w
-TERM screen.linux
-TERM screen.rxvt
-TERM terminator
-TERM vt100
-TERM vt220
-TERM xterm
-TERM xterm-16color
-TERM xterm-256color
-TERM xterm-88color
-TERM xterm-color
-TERM xterm-debian
-TERM xterm-kitty
-END
-sed -i 's/#force_color_prompt=yes/color_prompt=yes/g' ${chroot_dir}/home/ubuntu/.bashrc
-
-# Umount the temporary API filesystems
-umount -lf ${chroot_dir}/dev/pts 2> /dev/null || true
-umount -lf ${chroot_dir}/* 2> /dev/null || true
-
-# Tar the entire rootfs
-cd ${chroot_dir} && XZ_OPT="-0 -T0" tar -cpJf ../ubuntu-20.04-preinstalled-server-custom-arm64-apalis.rootfs.tar.xz . && cd ..
-
-images="ubuntu-20.04-preinstalled-server-arm64-apalis.rootfs.tar.xz ubuntu-20.04-preinstalled-server-custom-arm64-apalis.rootfs.tar.xz"
-for rootfs in ${images}; do
-    rm -rf ${chroot_dir}
-    mkdir -p ${chroot_dir}
-
-    # Untar the entire rootfs
-    cd ${chroot_dir} && tar -xpJf "../${rootfs}" . && cd ..
-
-    # Mount the temporary API filesystems
-    mkdir -p ${chroot_dir}/{proc,sys,run,dev,dev/pts}
-    mount -t proc /proc ${chroot_dir}/proc
-    mount -t sysfs /sys ${chroot_dir}/sys
-    mount -o bind /dev ${chroot_dir}/dev
-    mount -o bind /dev/pts ${chroot_dir}/dev/pts
-
-    # Download and update packages
-    cat << EOF | chroot ${chroot_dir} /bin/bash
 set -eE 
 trap 'echo Error: in $0 on line $LINENO' ERR
 
@@ -616,8 +376,8 @@ ubuntu-wallpapers
 apt-get -y autoremove && apt-get -y clean && apt-get -y autoclean
 EOF
 
-    # Service to start weston
-    cat > ${chroot_dir}/lib/systemd/system/weston.service << END
+# Service to start weston
+cat > ${chroot_dir}/lib/systemd/system/weston.service << END
 [Unit]
 Description=Weston Wayland Compositor (on tty7)
 RequiresMountsFor=/run
@@ -654,17 +414,12 @@ ExecStart=/usr/bin/weston --log=\${XDG_RUNTIME_DIR}/weston.log \$OPTARGS
 WantedBy=multi-user.target
 END
 
-    # Enable weston service
-    cat << EOF | chroot ${chroot_dir} /bin/bash
-set -eE 
-trap 'echo Error: in $0 on line $LINENO' ERR
+# Enable weston service
+chroot ${chroot_dir} /bin/bash -c "systemctl enable weston.service"
 
-systemctl enable weston.service
-EOF
-
-    # Configuration file for weston
-    mkdir -p ${chroot_dir}/etc/xdg/weston
-    cat > ${chroot_dir}/etc/xdg/weston/weston.ini << END
+# Configuration file for weston
+mkdir -p ${chroot_dir}/etc/xdg/weston
+cat > ${chroot_dir}/etc/xdg/weston/weston.ini << END
 [core]
 #gbm-format=argb8888
 idle-time=0
@@ -704,27 +459,26 @@ path=/usr/lib/chromium/chromium-bin --no-sandbox --use-gl=egl --enable-features=
 command=@bindir@/weston --backend=rdp-backend.so --shell=fullscreen-shell.so --no-clients-resize
 END
 
-    # Remove drm, mesa, and wayland
-    rm -rf ${chroot_dir}/usr/lib/aarch64-linux-gnu/libdrm*
-    rm -rf ${chroot_dir}/usr/lib/aarch64-linux-gnu/mesa-egl
-    rm -rf ${chroot_dir}/usr/lib/aarch64-linux-gnu/libglapi.so.0*
-    rm -rf ${chroot_dir}/usr/lib/aarch64-linux-gnu/libwayland-*
+# Remove drm, mesa, and wayland
+rm -rf ${chroot_dir}/usr/lib/aarch64-linux-gnu/libdrm*
+rm -rf ${chroot_dir}/usr/lib/aarch64-linux-gnu/mesa-egl
+rm -rf ${chroot_dir}/usr/lib/aarch64-linux-gnu/libglapi.so.0*
+rm -rf ${chroot_dir}/usr/lib/aarch64-linux-gnu/libwayland-*
 
-    # Extract and install GPU accelerated packages
-    for deb in ../debs/*/*.deb; do 
-        dpkg -x "${deb}" ${chroot_dir}
-    done
-
-    # Create links to shared libraries
-    chroot ${chroot_dir} /bin/bash -c "ldconfig"
-
-    # Gstreamer plugin search path
-    echo GST_PLUGIN_PATH="\"/usr/lib/gstreamer-1.0"\" >> ${chroot_dir}/etc/environment
-
-    # Umount the temporary API filesystems
-    umount -lf ${chroot_dir}/dev/pts 2> /dev/null || true
-    umount -lf ${chroot_dir}/* 2> /dev/null || true
-
-    # Tar the entire rootfs
-    cd ${chroot_dir} && XZ_OPT="-0 -T0" tar -cpJf "../${rootfs//server/desktop-weston}" . && cd ..
+# Extract and install GPU accelerated packages
+for deb in ../debs/*/*.deb; do 
+    dpkg -x "${deb}" ${chroot_dir}
 done
+
+# Create links to shared libraries
+chroot ${chroot_dir} /bin/bash -c "ldconfig"
+
+# Gstreamer plugin search path
+echo GST_PLUGIN_PATH="\"/usr/lib/gstreamer-1.0"\" >> ${chroot_dir}/etc/environment
+
+# Umount the temporary API filesystems
+umount -lf ${chroot_dir}/dev/pts 2> /dev/null || true
+umount -lf ${chroot_dir}/* 2> /dev/null || true
+
+# Tar the entire rootfs
+cd ${chroot_dir} && XZ_OPT="-0 -T0" tar -cpJf "../ubuntu-20.04-preinstalled-desktop-weston-arm64-apalis.rootfs.tar.xz" . && cd ..
